0dd631d10c71a4cc5da3b997d133edf0df32cc26
Zach Royer <zroyer@utexas.edu>
2015-08-13 18:50:50 -0500

    basic app with mock endpoints

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,10 +2,22 @@
 
 angular.module('subscriptionsApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+     
+    var testUrl = "http://private-anon-23e19a2f7-cratejoydev.apiary-mock.com/v1/subscriptions"
+	var url = "http://api.cratejoy.com/v1/subscriptions"
+	var id = "apitest"
+	var secret = "apitest"
+	var active = "ACTIVE"
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+    $http({
+    	method: "GET",
+    	url: testUrl,
+    	params: {
+    		status: active
+    	}
+    	
+    }).success(function (response) {
+    	$scope.results = response.results
     });
 
   });
